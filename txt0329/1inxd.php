76bd900b361ec8f58bf4e61a244034c7a60ba4fe
jackcaninhome <37394341+jackcaninhome@users.noreply.github.com>
2018-03-29 16:01:20 +0800

    Add files via upload

---
+++ txt0329/1inxd.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html>
+<html lang="zh_Hant">
+<head>
+	<meta charset ="utf-8">
+	<title >輸入</title>
+</head>
+<body>
+
+<form  method = "POST"  action="">
+請輸入商品名稱:<input type="text" name="prod" ><br>
+金額：<input type="text" name="price" ><br>
+<input  type = "submit">
+</form>
+
+</body>
+</html>
---
+++ txt0329/user.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機: 127.0.0.1
+-- 產生時間： 2018-03-29 09:50:11
+-- 伺服器版本: 10.1.25-MariaDB
+-- PHP 版本： 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `user`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `moneybook`
+--
+
+CREATE TABLE `moneybook` (
+  `m_id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(200) CHARACTER SET ascii NOT NULL,
+  `cost` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=big5;
+
+--
+-- 已匯出資料表的索引
+--
+
+--
+-- 資料表索引 `moneybook`
+--
+ALTER TABLE `moneybook`
+  ADD PRIMARY KEY (`m_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
